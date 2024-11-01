a79f9f478777f1cea03362d1b829718a89c81a0d
zchcpyw10 <571928489@qq.com>
2024-11-01 15:56:50 +0800

    add official example

---
+++ registry/new-york/example/button-with-icon.tsx
@@ -0,0 +1,11 @@
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons"
+
+import { Button } from "@/registry/new-york/ui/button"
+
+export default function ButtonWithIcon() {
+  return (
+    <Button>
+      <EnvelopeOpenIcon /> Login with Email
+    </Button>
+  )
+}
