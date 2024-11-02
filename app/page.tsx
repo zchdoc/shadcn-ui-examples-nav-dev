a0e1842f5fe9bd1a83461b421b299630b0688357
zchcpyw10 <571928489@qq.com>
2024-11-02 09:41:39 +0800

    format code nothing change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client"
 import { ModeToggle } from "@/components/theme-toggle";
-
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   const textTest = "shbds";
+  const goExamplePage = () => {
+    console.log("goExamplePage");
+    router.push('/examples/music')
+  };
   return (
     <div className="w-ful h-screen">
       <div className="flex">
         <ModeToggle />
+        <Button variant="link" onClick={goExamplePage}>Link</Button>
       </div>
     </div>
   );
